121da490e57d35deb080cf44ff65e0d55be5916c
Rafal Sarzynski <Rafcio199805@wp.pl>
2021-03-22 18:33:04 +0100

    fix: slight improvements to db and admin panel

--- cookbook/admin.py
+++ cookbook/admin.py
@@ -2,9 +2,25 @@ from django.contrib import admin
 
 from .models import Ingredient, RecipeStep, Recipe
 
-# todo rozbudowac to troszku
+
+class IngredientsInLine(admin.TabularInline):
+    model = Ingredient
+    extra = 2
+
+
+class RecipeStepsInLine(admin.TabularInline):
+    model = RecipeStep
+    extra = 2
+
+
+class RecipeStepAdmin(admin.ModelAdmin):
+    inlines = [IngredientsInLine]
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeStepsInLine]
+
 
 admin.site.register(Ingredient)
-admin.site.register(RecipeStep)
-admin.site.register(Recipe)
-
+admin.site.register(RecipeStep, RecipeStepAdmin)
+admin.site.register(Recipe, RecipeAdmin)
--- cookbook/models.py
+++ cookbook/models.py
@@ -2,6 +2,27 @@ from django.db import models
 
 
 # Create your models here.
+class Ingredient(models.Model):
+    ingredient_name = models.CharField(max_length=25)
+    price = models.FloatField()
+    kcal = models.IntegerField()
+    quantity = models.FloatField()
+    if_vegan = models.BooleanField(default=False)
+    recipe_step = models.ForeignKey('RecipeStep', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ingredient_name
+
+
+class RecipeStep(models.Model):
+    description = models.CharField(max_length=200)
+    time = models.IntegerField(default=0)
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
+
+
 class Recipe(models.Model):
     dish_name = models.CharField(max_length=25)
     average_time = models.IntegerField()
@@ -13,25 +34,4 @@ class Recipe(models.Model):
     )
 
     def __str__(self):
-        return self.dish_name + "Costs: " + self.average_price.__str__()
-
-
-class RecipeStep(models.Model):
-    description = models.CharField(max_length=200)
-    time = models.IntegerField(default=0)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.description + "Time: " + self.time.__str__()
-
-
-class Ingredient(models.Model):
-    ingredient_name = models.CharField(max_length=25)
-    price = models.FloatField()
-    kcal = models.IntegerField()
-    quantity = models.FloatField()
-    if_vegan = models.BooleanField(default=False)
-    recipe_step = models.ForeignKey(RecipeStep, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.ingredient_name + "Costs: " + self.price.__str__()
+        return self.dish_name
